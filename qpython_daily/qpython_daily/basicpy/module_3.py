0d26a74b4de4083b617fe53f6e10cfefe76d8bf3
Patrick <patrickwuke@163.com>
2022-08-22 11:48:13 +0800

    add python-30days-origin

---
+++ qpython_daily/qpython_daily/basicpy/module_3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from mymodule_3 import generate_full_name as fullname, sum_two_nums as total, person as p, gravity as g
+print(fullname('Asabneh','Yetayeh'))
+print(total(1, 9))
+mass = 100;
+weight = mass * g
+print(weight)
+print(p)
+print(p['firstname'])
